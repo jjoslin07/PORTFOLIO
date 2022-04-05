46d46a0543a58e89157a00418e39c203b648e15f
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2022-04-05 02:39:57 -0700

    : Font-Awesome Icons
    Add icons to homepage
    Style and position icons
    Add JavaScript to change active button and color
    Add and Remove 'actove' class from sections in app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll('.section');
+const sectionButtons = document.querySelectorAll('.controls');
+const sectionButton = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransitions() {
+	// Button click to select active class
+	for (let i = 0; i < sectionButton.length; i++) {
+		sectionButton[i].addEventListener('click', function () {
+			let currentButton = document.querySelectorAll('.active-btn');
+			currentButton[0].className = currentButton[0].className.replace(
+				'active-btn',
+				''
+			);
+			this.className += ' active-btn';
+		});
+	}
+	// Sections Active Class
+	allSections.addEventListener('click', (e) => {
+		const id = e.target.dataset.id;
+		if (id) {
+			// remove selected from the other buttons
+			sectionButtons.forEach((btn) => {
+				btn.classList.remove('active');
+			});
+			e.target.classList.add('active');
+
+			// hide other sections
+			sections.forEach((section) => {
+				section.classList.remove('active');
+			});
+
+			const element = document.getElementById(id);
+			element.classList.add('active');
+		}
+	});
+}
+
+PageTransitions();
